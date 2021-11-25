ded49de368f779d38a80181ddbb85e1d54e97d47
1000ch <shogosensui@gmail.com>
2021-11-25 15:19:39 +0900

    test: add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,18 @@
 import FluidIframe from '../src/index';
 
 describe('FluidIframe', (): void => {
+  beforeAll(() => {
+    customElements.define('fluid-iframe', FluidIframe);
+  });
+
   test('should be defined', (): void => {
     expect(FluidIframe).toBeDefined();
+    expect(customElements.get('fluid-iframe')).toBe(FluidIframe);
+  });
+
+  test('should update actual aspect ratio of <iframe>', () => {
+    const fluidIframe = new FluidIframe();
+    expect(fluidIframe).toBeInstanceOf(HTMLElement);
+    expect(fluidIframe.aspect).toBeUndefined();
   });
 })
